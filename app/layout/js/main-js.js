c901b740b4b50efb81004c279d5a50032b6f52d9
ManetX <manoelplneto456@gmail.com>
2021-04-07 21:29:13 -0300

    New version

--- app/layout/js/main-js.js
+++ app/layout/js/main-js.js
@@ -1,3 +1,30 @@
 function setValue(value){
-    document.getElementById("h1Value").innerHTML = value;
+
+    if(value == "+" || value == "-" || value == "*" || value == "/"){
+
+        console.log("Operacao");
+
+        var expressao = document.getElementById("h5Value").textContent;
+        var valor = document.getElementById("h1Value").textContent;
+
+        if((value == expressao.charAt(expressao.length-1))){
+
+            console.log("Operacao igual a ultima");
+
+
+        }else{
+
+            console.log("Operacao diferente da ultima");
+
+            document.getElementById("h5Value").innerHTML = document.getElementById("h5Value").textContent.concat(document.getElementById("h1Value").innerHTML, value);
+            document.getElementById("h1Value").innerHTML = "";
+
+        }
+
+    }else{
+
+        document.getElementById("h1Value").innerHTML = document.getElementById("h1Value").textContent.concat(value);
+
+    }
+
 }
