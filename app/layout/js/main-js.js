8365f21e7bceffa4ee9127ce8c8bf1c69969fa89
ManetX <manoelplneto456@gmail.com>
2021-04-08 19:25:06 -0300

    alteracoes

--- app/layout/js/main-js.js
+++ app/layout/js/main-js.js
@@ -1,30 +1,85 @@
+var resultadoParcial = 0;
+var entradaComParcial = true;
+
 function setValue(value){
 
     if(value == "+" || value == "-" || value == "*" || value == "/"){
 
+
         console.log("Operacao");
 
-        var expressao = document.getElementById("h5Value").textContent;
-        var valor = document.getElementById("h1Value").textContent;
+        if(!document.getElementById("h1Value").textContent == ""){
 
-        if((value == expressao.charAt(expressao.length-1))){
+            var resultadoParcial = document.getElementById("h1Parcial").textContent;
+            var expressao = document.getElementById("h5Value").textContent;
+            var operacaoAnterior = expressao.charAt(expressao.length-1);
+            var valorASerOperado = document.getElementById("h1Value").textContent;
 
-            console.log("Operacao igual a ultima");
+            // alert(resultadoParcial + operacaoAnterior + valorASerOperado);
+
+            // if(operacaoAnterior == "+"){
+
+            //     resultadoParcial = soma(resultadoParcial, valorASerOperado);
+
+            // }
 
 
-        }else{
 
-            console.log("Operacao diferente da ultima");
+            // alert(document.getElementById("h1Value").textContent);
+            // alert(operacaoAnterior);
+
+            // armazenaParcial(parseInt(valorASerOperado));
 
             document.getElementById("h5Value").innerHTML = document.getElementById("h5Value").textContent.concat(document.getElementById("h1Value").innerHTML, value);
             document.getElementById("h1Value").innerHTML = "";
+            document.getElementById("h1Value").innerHTML = soma(parseInt(valorASerOperado));
+            document.getElementById("h1Value").style = "color: orange;";
+
+            entradaComParcial = true;
+
+
 
         }
 
     }else{
 
-        document.getElementById("h1Value").innerHTML = document.getElementById("h1Value").textContent.concat(value);
+        if(value == "c"){
+
+            document.getElementById("h1Value").innerHTML = '&nbsp;';
+
+        }else if(value == "ce"){
+
+            document.getElementById("h5Value").innerHTML = '&nbsp;';
+            document.getElementById("h1Value").innerHTML = '&nbsp;';
+            resultadoParcial = 0;
+
+
+        }else{
+
+            if(entradaComParcial){
+
+                document.getElementById("h1Value").innerHTML = "";
+                entradaComParcial = false;
+                document.getElementById("h1Value").style = "color: dimgray;";
+                document.getElementById("h1Value").innerHTML = document.getElementById("h1Value").textContent.concat(value);
+
+            }else{
+
+                document.getElementById("h1Value").innerHTML = document.getElementById("h1Value").textContent.concat(value);
+            // entradaVazia = false;
+            }
+
+            
+
+
+        }
+
 
     }
 
+}
+
+function soma(valorASerOperado){
+
+    return resultadoParcial+=valorASerOperado;
 }
